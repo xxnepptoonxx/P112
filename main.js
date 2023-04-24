46c40c652f7577f32b0b4d572451e181565759cb
xxnepptoonxx <113563351+xxnepptoonxx@users.noreply.github.com>
2023-04-24 16:52:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+Webcam.set ({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach( '#camera ');
+
+function take_snapshot()
+{
+Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+});
+
+}
+
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/58ij--Klh/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+
+    else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        speak();
+        
+    }
+}
